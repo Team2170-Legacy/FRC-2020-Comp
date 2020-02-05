32b95d9e49d211f5d931f1714cda0a512c9f5230
Christopher Noll <christophernoll@Christophers-MacBook-Pro.local>
2020-02-04 21:05:46 -0500

    Added header for hardware config values

---
+++ src/main/include/hwcfg.h
@@ -0,0 +1,22 @@
+#pragma once
+
+enum CANIDs {
+	kLeftLeadID = 4,
+	kLeftFollowID = 3,
+	kRightLeadID = 1,
+    kRightFollowID = 2,
+	kShooterMotorID = 5
+};
+
+enum PCMIDs {
+    kHoodSolFwd = 0,
+    kHoodSolRev = 1
+};
+
+enum PWMIDs {
+
+};
+
+enum DIOIDs {
+
+};
