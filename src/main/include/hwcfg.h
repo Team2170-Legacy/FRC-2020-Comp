636923f75edbe2c4fc9cb24c7221154d32f5434e
KnockKnockSeven <memorybytes24@gmail.com>
2020-02-18 19:09:56 -0500

    defined methods

--- src/main/include/hwcfg.h
+++ src/main/include/hwcfg.h
@@ -8,13 +8,15 @@ enum CANIDs {
 	kShooterMotorID = 5,
 	kIntakeMotorID = 6,
 	kFeederMotorID = 8,
+	kClimberMotorID = 9
 };
 
 enum PCMIDs {
     kHoodSolFwd = 0,
     kHoodSolRev = 1,
 	kIntakeSolFwd = 2,
-	kIntakeSolRev = 3
+	kIntakeSolRev = 3,
+	kClimberSol = 4
 };
 
 enum PWMIDs {
