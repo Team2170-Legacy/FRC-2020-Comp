2cbd0a7c72324c58eecdcfde01d206dda3ef104f
Priyanshu4 <agpri123@gmail.com>
2020-02-23 12:04:33 -0500

    fixed unit issues with ramsete, still doesnt work

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -64,8 +64,8 @@ frc2::Command* RobotContainer::GenerateRamseteCommand() {
       // Start at the origin facing the +X direction
       frc::Pose2d(0_m, 0_m, frc::Rotation2d(0_deg)),
       // Pass through these two interior waypoints, making an 's' curve path
-//      {frc::Translation2d(1_m, 1_m), frc::Translation2d(2_m, -1_m)},
-      {},
+     {frc::Translation2d(1_m, 1_m), frc::Translation2d(2_m, -1_m)},
+      //{},
       // End 3 meters straight ahead of where we started, facing forward
       frc::Pose2d(3_m, 0_m, frc::Rotation2d(0_deg)),
       // Pass the config
@@ -77,7 +77,7 @@ frc2::Command* RobotContainer::GenerateRamseteCommand() {
     frc::RamseteController(AutoConstants::kRamseteB, AutoConstants::kRamseteZeta),
     DriveConstants::kDriveKinematics,
     [this](auto left, auto right){m_driveTrain.SetWheelVelocity(left, right);},
-    &m_driveTrain);
+    {&m_driveTrain});
 
   // An example command will be run in autonomous
   return new frc2::SequentialCommandGroup(
